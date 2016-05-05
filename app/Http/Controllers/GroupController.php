0487fd65048ed37a36c21f0e850ed70b96cf29fa
Sixuan Liu <sixuanliu@Sixuans-MBP-2.home>
2016-05-05 00:22:11 -0400

    add group endpoints

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -17,8 +17,8 @@ class GroupController extends Controller
     public function store(Request $request) {
         $input = $request->input();
         try{
-            $store = GroupModelSql::getInstance()->createGroup($input);
-            return self::buildResponse($store, self::SUCCESS_CODE);
+            $group = GroupModelSql::getInstance()->createGroup($input, self::$clientId);
+            return self::buildResponse(['group' => $group], self::SUCCESS_CODE);
 
         }catch (\Exception $e) {
             $content = array(
@@ -33,12 +33,35 @@ class GroupController extends Controller
     public function destroy(Request $request) {
         
     }
-    
+
+    //@todo add persons in the group response
     public function get($id) {
-        
+        try{
+            $group = GroupModelSql::getInstance()->getGroupById(self::$clientId, $id);
+            return self::buildResponse(['group' => $group], self::SUCCESS_CODE);
+
+        }catch (\Exception $e) {
+            $content = array(
+                'status' => self::GENERAL_BAD_RESPONSE_MESSAGE,
+                'message' => $e->getMessage(),
+                'error' => (string)$e
+            );
+            return self::buildResponse($content, self::BAD_REQUEST);
+        }
     }
 
-    public function index(Request $request) {
-        return response()->json(['name' => 'Abigail', 'state' => 'CA', 'client' => $this->getClientId()]);
+    public function index() {
+        try{
+            $groups = GroupModelSql::getInstance()->getGroupsByClientId(self::$clientId);
+            return self::buildResponse(['groups' => $groups], self::SUCCESS_CODE);
+
+        }catch (\Exception $e) {
+            $content = array(
+                'status' => self::GENERAL_BAD_RESPONSE_MESSAGE,
+                'message' => $e->getMessage(),
+                'error' => (string)$e
+            );
+            return self::buildResponse($content, self::BAD_REQUEST);
+        }
     }
 }
