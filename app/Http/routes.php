481df3403e0783761d9a1db6cec1d65d029207b9
Sixuan Liu <sixuanliu@Sixuans-MacBook-Pro-2.local>
2016-05-08 01:12:32 -0500

    route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,13 +28,13 @@ $app->group(['prefix' => 'faceApi', 'namespace' => 'App\Http\Controllers', 'midd
         $app->get('persons/{id}', 'PersonController@get');
         $app->delete('persons/{id}', 'PersonController@destroy');
             
-        $app->post('test', 'FaceController@socket');
-        $app->post('persons/faces', 'FaceController@store');
+        $app->post('test', 'FaceController@socket'); //testing-working
+        $app->post('persons/faces', 'FaceController@store'); //working
         $app->delete('persons/faces/{id}', 'FaceController@destroy');
         
         $app->post('detect', 'RecognitionController@detect');
-        $app->post('verify/{personId}', 'RecognitionController@verify');
-        $app->post('recognize', 'RecognitionController@recognize');
+        $app->post('verify/{personId}', 'RecognitionController@verify'); //working
+        $app->post('recognize/{groupId}', 'RecognitionController@recognize');
         $app->post('compare', 'RecognitionController@compare');
     }
 );
